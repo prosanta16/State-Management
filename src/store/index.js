b42926f6ee3d220f91df3eda7a5b9d0eca8f7164
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-20 00:34:57 +0600

    get data ,use getters

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import todos from './modules/todos'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 //create store
 export default new Vuex.Store({
     modules:{
         todos
     }
-})
+});
