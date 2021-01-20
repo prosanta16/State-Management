a3409606e8b1eeb2096b6c11bedc5e2f69cb9b09
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-20 19:45:10 +0600

    todo update

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -28,12 +28,15 @@ const actions={
           e.target.options[e.target.options.selectedIndex].innerText
         );
     
-        const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
-        );
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
     
         commit('setTodos', response.data);
-    }
+    },
+    async updateTodo({ commit }, updTodo) {
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,updTodo);
+        console.log( response.data);
+        commit('updateTodo', response.data);
+      }
     
   };
 
@@ -41,7 +44,13 @@ const actions={
 const mutations={
     setTodos:(state,todos) => (state.todos = todos),
     newTodo:(state,todo) => state.todos.unshift(todo),
-    removeTodo:(state,id) =>(state.todos=state.todos.filter(todo => todo.id !==id))
+    removeTodo:(state,id) =>(state.todos=state.todos.filter(todo => todo.id !==id)),
+    updateTodo:(state,updTodo) =>{
+        const index=state.todos.findIndex(todo => todo.id ===updTodo.id);
+        if(index!==-1){
+            state.todos.splice(index,1,updTodo);
+        }
+    }
 };
 
 
